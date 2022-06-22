40ee969d01e0f6eb283350e81a337efa2c3d36ec
Bonface Obilo <bonfaceobilo@gmail.com>
2022-06-22 12:14:13 +0300

    fix cors error

--- app.js
+++ app.js
@@ -15,6 +15,16 @@ require('dotenv/config')
 
 app.use(cors())
 app.options('*', cors())
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT')
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization'
+    )
+    next()
+});
 
 const api = process.env.API_URL
 
