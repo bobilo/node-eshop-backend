9328ed283f779c125bda4ae7d3dec84a32d51f8f
Bonface Obilo <bonfaceobilo@gmail.com>
2022-06-22 13:00:23 +0300

    fix cors

--- app.js
+++ app.js
@@ -13,18 +13,13 @@ const erroHandler = require('./helpers/error-handler')
 
 require('dotenv/config')
 
-app.use(cors())
+app.use(
+    cors({
+        origin: 'http://localhost:4200',
+        credentials: true,
+    })
+)
 app.options('*', cors())
-app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*')
-    res.header('Access-Control-Allow-Origin', '*')
-    res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT')
-    res.header(
-        'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization'
-    )
-    next()
-});
 
 const api = process.env.API_URL
 
