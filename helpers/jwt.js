07ee5c89cf5339e73232f14563ed2f132ccc62a8
Bonface Obilo <bonfaceobilo@gmail.com>
2022-06-22 12:51:04 +0300

    fix orders count urls

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -14,7 +14,7 @@ function authJwt() {
             { url: /\/api\/v1\/categories(.*)/, methods: ['GET', 'OPTIONS'] },
             {
                 url: /\/api\/v1\/orders(.*)/,
-                methods: ['POST', 'OPTIONS'],
+                methods: ['GET', 'OPTIONS'],
             },
             `${api}/users/login`,
             `${api}/users/register`,
